e4cee01783557daf4691dc14065403ddf0c482bf
Phil Bolduc <philbolduc@gmail.com>
2020-09-19 11:18:24 -0700

    Tried to create a average implementation not working

--- wled00/usermod_latency.h
+++ wled00/usermod_latency.h
@@ -4,40 +4,75 @@
 
 #define ELAPSED(current, previous) ((previous) <= (current) ? (current) - (previous) : (UINT32_MAX - previous) + current)
 
+#define SAMPLE_COUNT 1000
+
 class UsermodLatency : public Usermod {
   private:
-    unsigned long last;
-    unsigned long latency;
+    unsigned long samples[SAMPLE_COUNT];
+    unsigned long total = 0;
+    unsigned long samples_count = 0;
 
-    unsigned long latencyMax;
+    // the previous micros value
+    unsigned long previous = 0;
+    // true if we have populated all SAMPLE_COUNT samples
+    bool full_samples = false;
+    bool firstLoop = true;
+
+    unsigned long lastPublish = 0;
 
   public:
     void setup() {
-      last = micros();
     }
 
     void loop() {
-      unsigned long now = micros();
-      latency = ELAPSED(now, last);
-      // only measure the max latency after we have been booted for 2 seconds
-      if (latencyMax < latency && 2000000 <= now ) {
-        latencyMax = latency;
+      unsigned long current = micros();
+
+      if (firstLoop) {
+        // need to capture the first loop time on the first 
+        // loop call. cant capture in setup due to other setup
+        // additional latency
+        firstLoop = false;
+      } else {
+        // calculate the time between loop() calls
+        unsigned long sample = ELAPSED(current, previous);
+
+        samples[samples_count++] = sample;
+        total += sample;
+
+        // just keep accumulating the samples until our rolling
+        // average buffer is full
+        if (!full_samples) {
+          full_samples = (samples_count == SAMPLE_COUNT);
+        } else {
+          // subtract the oldest value from the total
+          total -=  samples[samples_count % SAMPLE_COUNT];
+        }
       }
-      last = now;
+
+      previous = current;
+
+
+      unsigned long now = millis();
+      // publish every 5 seconds
+      if (now - lastPublish < 5000)
+      {
+        return;
+      }
+
+      lastPublish = now;
+
+      if (WLED_MQTT_CONNECTED) {
+          float average = (1.0/SAMPLE_COUNT)  * total;
+
+          char topic[38];
+          strcpy(topic, mqttDeviceTopic);
+          strcat(topic, "/loop-latency");
+          mqtt->publish(topic, 0, true, String(average).c_str());
+        }
     }
 
     void addToJsonInfo(JsonObject& root) {
-      JsonObject user = root["u"];
-      if (user.isNull()) user = root.createNestedObject("u");
-
-      JsonArray temp = user.createNestedArray("Latency");
-      temp.add(latency);
-      temp.add(" μs");
-
-      temp = user.createNestedArray("Max Latency");
-      temp.add(latencyMax);
-      temp.add(" μs");
-      return;
+      // do not write to info
     }
 
     uint16_t getId()
